3b1d87a30b07b0e98239b34ecfe42a61f42e1846
RJ Gonato <53969477+rjaygonato@users.noreply.github.com>
2024-08-22 08:37:31 +0800

    Updating JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
 /**
-* Template Name: DevFolio - v4.7.1
-* Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
+* Author: RJ GONATO
 */
 (function() {
   "use strict";
